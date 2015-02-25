6dc2fe8648bc6527602eb6407de560d48fb3c1a9
1KRASNIQIARI <1KRASNIQIARI@gws33873.ihdsb.ca>
2015-02-25 10:59:00 -0500

    -Creating a Bingo Table with table format

---
+++ src/edu/hdsb/gwss/krasniqi/arian/ics3u/u3/BingoCard.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.hdsb.gwss.krasniqi.arian.ics3u.u3;
+
+/**
+ *
+ * @author 1KRASNIQIARI
+ */
+public class BingoCard {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.format("%5s %5s %5s %5s %5s\n","B","I","N","G","0");
+        System.out.format("%5s %5s %5s %5s %5s\n","10","23","44","16","28");
+        System.out.format("%5s %5s %5s %5s %5s\n","12","18","Free","22","63");
+        System.out.format("%5s %5s %5s %5s %5s\n",9,28,84,36,68);
+        System.out.format("%5s %5s %5s %5s %5s\n",19,2,4,45,67);
+    }
+    
+    
+}
