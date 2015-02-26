02321cfb7a879e3f2b856cf311baf4c8c4b0315f
1krasniqiari <1krasniqiari@gws33873.ihdsb.ca>
2015-02-26 11:08:11 -0500

    - Shortfall activity

---
+++ src/edu/hdsb/gwss/krasniqi/arian/ics3u/u3/Shortfall.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.hdsb.gwss.krasniqi.arian.ics3u.u3;
+
+/**
+ *
+ * @author 1krasniqiari
+ */
+public class Shortfall {
+
+    /**
+     * @param args the command line arguments
+     */
+        // TODO code application logic here
+  public static void main ( String[] args ) {
+    int value = 35000;
+    System.out.println("A short: " +  value);
+  }
+}
+        
+        
+
+    
+
