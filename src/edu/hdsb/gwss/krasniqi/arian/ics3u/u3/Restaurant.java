e71af0d517cd696f354ce98f3b2be600b680aa52
1krasniqiari <1krasniqiari@gws33873.ihdsb.ca>
2015-02-27 11:08:15 -0500

    restouarant activity

---
+++ src/edu/hdsb/gwss/krasniqi/arian/ics3u/u3/Restaurant.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.hdsb.gwss.krasniqi.arian.ics3u.u3;
+
+/**
+ *
+ * @author 1krasniqiari
+ */
+public class Restaurant {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    int drink = 3;
+    int main = 15;
+    int dessert = 10;
+    double hst1 = 0.13;
+    double hst = (hst1 * total);
+    double total = (drink + main + dessert);
+    
+    System.out.println ("Drink" +   "           $"+drink);
+    System.out.println ("Main Course" +   "     $"+main);
+    System.out.println ("Dessert" +   "         $"+dessert);
+    System.out.println ("Your Total is " + "  $"+ hst);
+           
+         
+           
+    
+    
+    
+    }
+    
+}
