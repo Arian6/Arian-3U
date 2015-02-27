088aed753537edb4f80263f8664165e08733059a
1krasniqiari <1krasniqiari@gws33873.ihdsb.ca>
2015-02-27 11:09:12 -0500

    perimeter

---
+++ src/edu/hdsb/gwss/krasniqi/arian/ics3u/u3/AreaPerimeter.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.hdsb.gwss.krasniqi.arian.ics3u.u3;
+
+/**
+ *
+ * @author 1krasniqiari
+ */
+public class AreaPerimeter {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+            int length = 7;
+            int width = 8;
+            int perimeter = length * 2 + width * 2;
+            
+            System.out.println ("The perimeter is " + perimeter);
+            
+    
+    
+    }
+    
+    
+}
