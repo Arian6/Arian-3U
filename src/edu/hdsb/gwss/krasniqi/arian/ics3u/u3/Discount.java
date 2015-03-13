211d11f37613d1089dda109bb952c7b721600d26
1krasniqiari <1krasniqiari@gws33873.ihdsb.ca>
2015-03-13 10:18:38 -0400

    - discount done

--- src/edu/hdsb/gwss/krasniqi/arian/ics3u/u3/Discount.java
+++ src/edu/hdsb/gwss/krasniqi/arian/ics3u/u3/Discount.java
@@ -1,5 +1,6 @@
 package edu.hdsb.gwss.krasniqi.arian.ics3u.u3;
 
+import java.text.NumberFormat;
 import java.util.Scanner;
 
 /*
@@ -8,8 +9,8 @@ import java.util.Scanner;
  * and open the template in the editor.
  */
 
-/**
- *
+/**Discount assignment
+ * Version: 0.2
  * @author 1krasniqiari
  */
 public class Discount {
@@ -18,24 +19,35 @@ public class Discount {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // TODO code application logic her
+        
+        
+        //Scanner Input
         Scanner input = new Scanner( System.in );
         int amountofp;
         
-            System.out.println("Enter the amount");
-        
+            System.out.println("Enter the amount of items you have purchased:");
+   //Variable     
    amountofp = input.nextInt();
    
+    //Double variable
    double discountc = (amountofp * .1);
    double discounta = (amountofp - discountc);
+   
+   //If statement for ineligability
     if (amountofp <= 9) {
     System.out.println( "Not eligible for discount, Your total is   $" + amountofp);
     }
-    
+   //Eligiblel for discount
     else {
     System.out.println("You are eligible for our 10% discount!");
-    System.out.println("Your total is   $" + discounta);
+    System.out.println("Your total is");
             }
+    
+       // Money (localized)
+NumberFormat money = NumberFormat.getCurrencyInstance();
+double dollars = discounta;
+System.out.println( money.format( dollars ) );
         
         
         
