0e679ebd5c1c39331e32d4d828525412a45c4cab
ARIAN <ARIAN@192.168.1.107>
2015-03-22 18:05:05 -0400

    - all assignment update 4

--- src/edu/hdsb/gwss/krasniqi/arian/ics3u/u3/AlienDetector.java
+++ src/edu/hdsb/gwss/krasniqi/arian/ics3u/u3/AlienDetector.java
@@ -18,42 +18,37 @@ public class AlienDetector {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
+
         //objects
         Scanner input = new Scanner(System.in);
-        
+
         //Variables
         int antenna;
-        int eyes;  
-              
+        int eyes;
+
         //Code
         System.out.println("How many antennas did the alien have?");
-        
-          antenna = input.nextInt();
-          
-       
-        
-         System.out.println("How many eyes did the alien have?");
-        
-          eyes = input.nextInt();
-        
+
+        antenna = input.nextInt();
+
+        System.out.println("How many eyes did the alien have?");
+
+        eyes = input.nextInt();
+
           // If statements
-          
-          if (antenna >= 3 & eyes <=4) {
-              System.out.println("The alien you saw could be the TroyMartian");
-          }
-        
-          if (antenna <= 6 & eyes >=2) {
-              System.out.println("The alien you saw could be the VladSaturnian");
-          }
-          if (antenna >= 2 & eyes <=3) {
-              System.out.println("The alien you swa could be the GraemeMercurian");
-          } else {
-              System.out.println("Unidentified species");
-          }
-        
-        
-        
+        if (antenna >= 3 & eyes <= 4) {
+            System.out.println("The alien you saw could be the TroyMartian");
+        }
+
+        if (antenna <= 6 & eyes >= 2) {
+            System.out.println("The alien you saw could be the VladSaturnian");
+        }
+        if (antenna >= 2 & eyes <= 3) {
+            System.out.println("The alien you saw could be the GraemeMercurian");
+        } else {
+            System.out.println("Unidentified species");
+        }
+
     }
-    
+
 }
